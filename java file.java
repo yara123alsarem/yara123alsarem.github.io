028ef81965cf39a8928e4cbcf3fd1e98e584a777
yara123alsarem <49021211+yara123alsarem@users.noreply.github.com>
2019-03-28 22:28:52 +0200

    Add files via upload

---
+++ java file.java
@@ -0,0 +1,15 @@
+public classJavafile
+public file()
+{
+	
+	super();
+	x=10;
+}
+	public void print()
+	{
+	system.out.printLine("java file");
+}
+publicvoid print2()
+{
+	system.out.printLine("java file2");
+}
